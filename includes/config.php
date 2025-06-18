a8a8566692681ee6071ec675d4a01ec3690e413e
Edem Kojo Agbakpe <edem.agbakpe@gmail.com>
2025-06-18 13:08:39 +0000

    setup homepage and vercel config for consulting site

--- includes/config.php
+++ includes/config.php
@@ -12,12 +12,12 @@
 |
 |
 */
-  define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', '' );             // Set database password
-  define( 'DB_NAME', 'restodb' );        // Set database name
+define('DB_HOST', 'localhost');          // Set database host
+define('DB_USER', 'root');             // Set database user
+define('DB_PASS', '');             // Set database password
+define('DB_NAME', 'restodb');        // Set database name
 
 date_default_timezone_set("Africa/Accra");
 $currentDateTime = new DateTime('now');
-$currentDate = $currentDateTime->format('Y-m-d\TH:i'); 
+$currentDate = $currentDateTime->format('Y-m-d\TH:i');
 ?>
